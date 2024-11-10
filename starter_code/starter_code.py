1be1181549ef5938de6524bb04443bf8e0a0debb
Tanush Yarram <t.yarram@tamu.edu>
2024-11-10 11:57:14 -0600

    openai model

--- starter_code/starter_code.py
+++ starter_code/starter_code.py
@@ -1,4 +1,6 @@
 from gensim.models import KeyedVectors
+import openai
+openai.api_key = 'your-api-key'
 
 print("loading model")
 # Load the Google News Word2Vec model
@@ -7,6 +9,17 @@ model_path = 'archive/GoogleNews-vectors-negative300.bin'
 word2vec_model = KeyedVectors.load_word2vec_format(model_path, binary=True)
 print("model loaded")
 
+
+def get_similarity(word1, word2):
+    response = openai.Embedding.create(
+        input=[word1, word2],
+        model="text-embedding-ada-002"
+    )
+    embedding1 = response['data'][0]['embedding']
+    embedding2 = response['data'][1]['embedding']
+    
+    similarity = sum(a*b for a, b in zip(embedding1, embedding2)) / (sum(a**2 for a in embedding1)**0.5 * sum(b**2 for b in embedding2)**0.5)
+    return similarity
 
 def model(words, strikes, isOneAway, correctGroups, previousGuesses, error):
 	"""
@@ -38,12 +51,15 @@ def model(words, strikes, isOneAway, correctGroups, previousGuesses, error):
 					if(words[i] in correctGroups or words[j] in correctGroups or words[k] in correctGroups or words[l] in correctGroups):
 						continue
 					group = [words[i], words[j], words[k], words[l]]
-					score = (word2vec_model.similarity(words[i], words[j]) + word2vec_model.similarity(words[i], words[k]) + 
-							word2vec_model.similarity(words[i], words[l]) + word2vec_model.similarity(words[j], words[k]) + 
-							word2vec_model.similarity(words[j], words[l]) + word2vec_model.similarity(words[k], words[l])) / 6
+					score = 0
+					for m in range(4):
+						for n in range(m + 1, 4):
+							score += get_similarity(group[m], group[n])
+					score /= 6
 					if score > highestScore and group not in previousGuesses:
 						nextGuess = group
 						highestScore = score
+
 	
 	# guess = ["apples", "bananas", "oranges", "grapes"] # 1D Array with 4 elements containing guess
 	endTurn = False # True if you want to end puzzle and skip to the next one
